0e264cf919d40a9ef649efcb346a464421acafbe
ghesketh76 <ghesketh76@gmail.com>
2021-02-18 21:14:07 -0700

    post new round

--- dashboard.js
+++ dashboard.js
@@ -1,9 +1,10 @@
 const baseURL = "http://localhost:3000"
 const courseURL = `${baseURL}/golfcourses`
-const teeBoxURL = `${baseURL}/teeboxes`
+const newRoundURL = `${baseURL}/newrounds`
 
 const courseDropDown = document.querySelector('#course-dropdown')
 const teeDropDown = document.querySelector('#teebox-dropdown')
+const newRoundForm = document.querySelector('.new-round')
 
 fetch(courseURL)
     .then(response => response.json())
@@ -12,21 +13,24 @@ fetch(courseURL)
         courseOption.value = course.id
         courseOption.textContent = course.name
         courseDropDown.append(courseOption)
-        console.log(course.teeboxes)
-        // const teeOption = document.createElement('option')
-        // teeOption.value = course.teeboxes.id
-        // console.log(teeOption)
-
-    //   courseDropDown.addEventListener('change', (event) => {
-    //       addTeeBoxes(event, course)
-    //     })
     }))
 
-// function addTeeBoxes(event, course) {
-//     if (event.target.value === "7") {
-//         // const currentCourse = course.find(course => event.target.value === course.id)
-//         console.log(course)
-//     }
-// }
+newRoundForm.addEventListener('submit', postNewRound)
 
-// console.log(courseDropDown.value)
+function postNewRound (event) {
+    event.preventDefault()
+    const newRoundData = new FormData(event.target)
+    const golfcourse_id = newRoundData.get('golfcourse_id')
+    const score = newRoundData.get('score')
+    const newRound = {golfcourse_id, score}
+   
+    fetch(newRoundURL, {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${localStorage.token}`,
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(newRound)
+    }).then(response => response.json())
+        .then(round => console.log(round))
+}
